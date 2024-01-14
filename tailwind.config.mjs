b5349167263260b14d7f2289a6e0072b26ca858d
Alexander Urrutia <matias04041994@gmail.com>
2024-01-14 15:56:43 -0300

    Se agrega sección principal

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,8 +3,19 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontSize: {
-      "2xs": "11px",
-      xs: "14px",
+      "2xs": "0.688rem",
+      xs: "0.875rem",
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+      "6xl": "3.400rem",
+    },
+    fontFamily: {
+      pacifico: ["Pacifico", "regular"],
     },
   },
   plugins: [],
