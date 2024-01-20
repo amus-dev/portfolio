455ec1479b205a01743d2acef2a6ba809bada63b
Alexander Urrutia <matias04041994@gmail.com>
2024-01-20 16:01:41 -0300

    adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   image: {
-   service: passthroughImageService(),
- },
+    service: passthroughImageService(),
+  },
 });
