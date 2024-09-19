1989d2d7f7b28793a855738141d3509007431c69
Alexander Urrutia <matias04041994@gmail.com>
2024-09-19 15:53:45 -0300

    Add @tailwindcss/typography plugin to tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -18,5 +19,5 @@ export default {
       pacifico: ["Pacifico", "regular"],
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
